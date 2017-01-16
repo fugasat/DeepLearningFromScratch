bdf8038f4ec0ab174f5d5d3b3bd9b50294adc8d9
Satoru Takahashi <fugasat@gmail.com>
2017-01-16 12:43:51 +0900

    -p75 サンプル画像を表示

--- chap3/mnist.py
+++ chap3/mnist.py
@@ -1,11 +1,22 @@
 import sys, os
 sys.path.append(os.pardir)
+import numpy as np
 from dataset.mnist import load_mnist
+from PIL import Image
+
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
 
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+img = x_train[0]  # 28 x 28 [gray]
+label = t_train[0]
+print(label)
 
-print(x_train.shape)
-print(t_train.shape)
-print(x_test.shape)
-print(t_test.shape)
+print(img.shape)
+img = img.reshape(28, 28)  # 2次元配列[28,28]に変換
+print(img.shape)
 
+img_show(img)
