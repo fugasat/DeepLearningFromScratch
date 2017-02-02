024d56e351438151f3e47d532981c5b55e7ade6a
Satoru Takahashi <fugasat@gmail.com>
2017-02-02 13:00:41 +0900

    -p100 サンプルグラフを描画（微分学習用）

--- chap4/nn.py
+++ chap4/nn.py
@@ -1,6 +1,7 @@
 import sys, os
 import numpy as np
 from dataset.mnist import load_mnist
+import matplotlib.pylab as plt
 
 
 def mean_squared_error(y, t):
@@ -26,7 +27,7 @@ def cross_entropy_error_one_hot_false(y, t):
     return -np.sum(np.log(y_arange)) / batch_size
 
 
-if __name__ == '__main__':
+def batch_sample():
     # x 0から9の値
     # t 画素データ
     (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
@@ -35,5 +36,28 @@ if __name__ == '__main__':
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
-    error = cross_entropy_error(x_batch, t_batch)
+
+
+def numerical_diff(f, x):
+    h = 1e-4 # 0.0001
+    d = (f(x + h) - f(x - h)) / (2 * h)
+    return d
+
+
+def function_1(x):
+    return 0.01 * x ** 2 + 0.1 * x
+
+
+def numerical_diff_sample():
+    x = np.arange(0.0, 20.0, 0.1)  # 0.0,0.1,0.2, ... , 20.0
+    y = function_1(x)
+    plt.xlabel("x")
+    plt.ylabel("f(x)")
+    plt.plot(x, y)
+    plt.savefig('graph_4_6.png')
+    pass
+
+
+if __name__ == '__main__':
+    numerical_diff_sample()
     pass
