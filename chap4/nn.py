d3e53c75dad1bf840eed6572256d8ed61773bde9
Satoru Takahashi <fugasat@gmail.com>
2017-02-03 13:15:13 +0900

    -p102 3Dグラフを描画

--- chap4/nn.py
+++ chap4/nn.py
@@ -2,6 +2,7 @@ import sys, os
 import numpy as np
 from dataset.mnist import load_mnist
 import matplotlib.pylab as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def mean_squared_error(y, t):
@@ -48,6 +49,10 @@ def function_1(x):
     return 0.01 * x ** 2 + 0.1 * x
 
 
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+
 def numerical_diff_sample():
     x = np.arange(0.0, 20.0, 0.1)  # 0.0,0.1,0.2, ... , 20.0
     y = function_1(x)
@@ -68,9 +73,50 @@ def numerical_diff_sample():
 
     plt.savefig('graph_4_7.png')
 
-    pass
+
+def _numerical_gradient_no_batch(f, x):
+    h = 1e-4  # 0.0001
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)  # f(x+h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)  # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+
+        x[idx] = tmp_val  # 値を元に戻す
+
+    return grad
+
+
+def numerical_gradient(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_no_batch(f, X)
+    else:
+        grad = np.zeros_like(X)
+
+        for idx, x in enumerate(X):
+            grad[idx] = _numerical_gradient_no_batch(f, x)
+
+        return grad
+
+
+def numerical_diff_sample2():
+    x0 = np.arange(-3, 3, 0.25)
+    x1 = np.arange(-3, 3, 0.25)
+    X, Y = np.meshgrid(x0, x1)
+    Z = function_2(np.array([X, Y]))
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.plot_wireframe(X, Y, Z)
+
+    plt.savefig('graph_4_8.png')
 
 
 if __name__ == '__main__':
-    numerical_diff_sample()
+    numerical_diff_sample2()
     pass
