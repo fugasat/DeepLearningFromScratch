4dc7525ac45791482f4b2baea38fe0c937c77348
Satoru Takahashi <fugasat@gmail.com>
2017-03-01 12:57:13 +0900

    -p139 乗算＆加算レイヤ実装

---
+++ chap5/buy_apple_orange.py
@@ -0,0 +1,17 @@
+from chap5.mul_layer import MulLayer
+from chap5.add_layer import AddLayer
+
+apple = 100
+apple_num = 2
+orange = 150
+orange_num = 3
+tax = 1.1
+
+# layer
+mul_apple_layer = MulLayer()
+mul_orange_layer = MulLayer()
+add_apple_orange_layer = AddLayer()
+mul_tax_layer = MulLayer()
+
+# forward
+apple_price = mul_apple_layer.forward(apple, apple_num)
