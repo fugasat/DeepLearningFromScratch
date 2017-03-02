fb2ef567756cf3ef8bcb642a936e102536c88ec2
Satoru Takahashi <fugasat@gmail.com>
2017-03-02 12:33:16 +0900

    -p141 加算レイヤの実装

--- chap5/buy_apple_orange.py
+++ chap5/buy_apple_orange.py
@@ -15,3 +15,17 @@ mul_tax_layer = MulLayer()
 
 # forward
 apple_price = mul_apple_layer.forward(apple, apple_num)
+orabge_price = mul_orange_layer.forward(orange, orange_num)
+all_price = add_apple_orange_layer.forward(apple_price, orabge_price)
+price = mul_tax_layer.forward(all_price, tax)
+
+# backward
+dprice = 1
+dall_price, dtax = mul_tax_layer.backward(dprice)
+dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)
+dorange, dorange_num = mul_orange_layer.backward(dorange_price)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+
+print(price)
+print(dapple_num, dapple, dorange, dorange_num, dtax)
+
